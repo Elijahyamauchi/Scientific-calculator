1a487949909bbef947c940ea52a9810d205ee225
dascil <zacharydascil@protonmail.com>
2023-10-18 16:45:26 -0700

    Added a basic arithmetic class

---
+++ BasicArithmeticFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculatorFunctions
+{
+	public class BasicArithmeticFunctions
+	{
+		public static double Addition(double a, double b)
+		{
+			return a + b;
+		}
+
+		public static double Subtraction(double a, double b)
+		{
+			return a - b;	
+		}
+
+		public static double Multiplication(double a, double b) { 
+			return a * b;
+		}
+
+		public static double Division(double a, double b)
+		{
+			return b == 0 ? double.NaN : a / b;
+		}
+	}
+}
